5113549632b8198b3abbbb34c82680a0b7d3a473
nestfahd <mushek.fahath@nestgroup.net>
2023-04-26 15:11:43 +0530

    demo

--- src/test/java/com/steps/Stepdefination.java
+++ src/test/java/com/steps/Stepdefination.java
@@ -1,31 +1,33 @@
 package com.steps;
 
+import org.junit.Test;
+
 import com.exeute.Pageexecution;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class Stepdefination extends Pageexecution {
-
+	@Test
 	@Given("Launch chrome browser")
 	public void launch_chrome_browser() {
 		launchChrome();
 	    
 	}
-
+	@Test
 	@When("Navigate to demoAPP url")
 	public void navigate_to_demo_app_url() {
 		launchDemoAPP();
 	   
 	}
-
+	@Test
 	@Given("Enter username and password")
 	public void enter_username_and_password() throws InterruptedException {
 		enterUsername();
 		enterPassword();
 		
 	}
-
+	@Test
 	@When("Click Login button")
 	public void click_login_button() {
 		clickLoginButton();
